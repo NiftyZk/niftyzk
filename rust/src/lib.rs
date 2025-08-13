024b36af7c3665753d13746e821abdddd9b4f6b7
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-08-14 01:44:25 +0200

    I literally added plonkit!

---
+++ rust/src/lib.rs
@@ -0,0 +1,28 @@
+#![allow(clippy::unit_arg)]
+
+#[macro_use]
+extern crate serde;
+#[macro_use]
+extern crate hex_literal;
+extern crate bellman_vk_codegen;
+extern crate byteorder;
+extern crate franklin_crypto;
+extern crate itertools;
+extern crate num_bigint;
+extern crate num_traits;
+extern crate rand;
+extern crate anyhow;
+
+
+pub mod circom_circuit;
+pub mod plonk;
+pub mod r1cs_file;
+pub mod reader;
+
+pub mod transpile;
+pub mod utils;
+
+pub use franklin_crypto::bellman as bellman_ce;
+
+#[cfg(test)]
+mod tests;
